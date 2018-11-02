a8e124c2eb20b7262c7ded30d9e33b08817910f1
Romain <rmtestard@gmail.com>
2018-11-02 15:40:47 +0100

    react frontend on heroku

---
+++ src/main/resources/static/precache-manifest.5f56d3cb5b307ca7e1a88dee09af1dfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b48c176ab92a7f0443f3",
+    "url": "/static/js/main.b48c176a.chunk.js"
+  },
+  {
+    "revision": "1cc616242c9756f6e60e",
+    "url": "/static/js/1.1cc61624.chunk.js"
+  },
+  {
+    "revision": "b48c176ab92a7f0443f3",
+    "url": "/static/css/main.fdd031f1.chunk.css"
+  },
+  {
+    "revision": "1cc616242c9756f6e60e",
+    "url": "/static/css/1.dee9975c.chunk.css"
+  },
+  {
+    "revision": "67df524fd47a397800a235871f819826",
+    "url": "/index.html"
+  }
+];
